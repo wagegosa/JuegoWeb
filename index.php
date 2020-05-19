57e2c2cefea076a9b26311a4b4deb43b8ecdd516
Gerardo González <wagegosa@gmail.com>
2020-05-19 03:02:05 -0500

    Se actualiza los datos princiáles para poder visualizar el ejemplo.

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <title>Video Juego Web</title>
+  <meta name="description" conent="Se realiza ejecicio de juego para seguir practicando JavaSacript">
+  <meta name="author" conent="Gerardo González">
+</head>
+<body>
+  <h1>¡Hola Mundo!</h1>
+</body>
+</html>
