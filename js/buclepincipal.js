c955e214aafb792cbbce01b43ee1c1334c72df22
Gerardo González <wagegosa@gmail.com>
2020-05-19 17:18:06 -0500

    Se crean funciones y se le llaman paraa actyalizar, detener y dibujar y dentro de iterar se crea condicionar para realizar un contador.

--- js/buclepincipal.js
+++ js/buclepincipal.js
@@ -11,9 +11,20 @@ var buclePrincipal = {
   fps: 0,
   // creamos una función para repetir  o iterar
   iterar: function(registroTemporal){
+    
     // llammos el atributo idEjecución y luego solicitamos el cuadro de animación 
     // la función window.requestAnimationFrame vuelve y llama la función iterar con el fin de siempres estar actualizado en la parte de animación
     buclePrincipal.idEjecución = window.requestAnimationFrame(buclePrincipal.iterar);
+    // llamamos actualizar y dibujar
+    buclePrincipal.actualizar(registroTemporal);
+    buclePrincipal.dibujar();
+    // creamos una condicional para realizar un contador de APS y FPS
+    if(registroTemporal - buclePrincipal.ultimoRegistro > 999){
+      buclePrincipal.ultimoRegistro = registroTemporal;
+      console.log("APS: " + buclePrincipal.aps + " | FPS: " + buclePrincipal.fps);
+      buclePrincipal.aps = 0;
+      buclePrincipal.fps = 0;
+    }
   },
   // funcion para detener el juego
   detener: function(){},
