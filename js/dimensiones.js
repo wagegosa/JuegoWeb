3c909a44de196f68f55c405730a5f716c9965b08
Gerardo González <wagegosa@gmail.com>
2020-05-20 03:29:04 -0500

    Se crea archivo donde se ejecuta una funcion para ver el ancho y alto de una patalla, esto se actualiza constantemente.

---
+++ js/dimensiones.js
@@ -0,0 +1,12 @@
+var dimensiones = {
+    ancho: window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth,
+    alto: window.innerHeight || document.documentElement.clientHeight ||document.body.clientHeight,
+    iniciar: function(){
+        window.addEventListener("resize", function
+        (e){
+            dimensiones.ancho = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+            dimensiones.alto = window.innerHeight || document.documentElement.clientHeight ||document.body.clientHeight;
+            console.log("Ancho: " + dimensiones.ancho + ", Alto: " + dimensiones.alto);
+        });
+    }
+};
