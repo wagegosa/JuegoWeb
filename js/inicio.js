a0cfa1146123782e0d104f04f5a1c435a10d8c37
Gerardo González <wagegosa@gmail.com>
2020-05-22 15:33:59 -0500

    Se crea llamado funcion recargarTitles e igual se creata esta función donde armamos una matriz y que contiene un todo Rectangulo y elviamos las dimenciones x, y, alto y ancho.

--- js/inicio.js
+++ js/inicio.js
@@ -1,17 +1,24 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function () {
   inicio.iniciarJuego();
 }, false);
 
 // Control + f5, sirve para recargar pagina pero limpiando la cache 
-
-var inicio ={
-  iniciarJuego: function(){
+var inicio = {
+  iniciarJuego: function () {
     console.log("Juego iniciado");
     dimensiones.iniciar()
-    var r = new Rectangulo(0, 0, 100, 100);
-    var r2 = new Rectangulo(100, 0, 100, 100);
-    var r3 = new Rectangulo(200, 0, 100, 100);
+    //var r = new Rectangulo(0, 0, 100, 100);
+    // var r2 = new Rectangulo(100, 0, 100, 100);
+    inicio.recargarTiles();
     buclePrincipal.iterar();
-
+  },
+  recargarTiles: function () {
+    document.getElementById("juego").innerHTML = "";
+    for (var y = 0; y < dimensiones.obtenerTilesVerticales(); y++) {
+      for (var x = 0; x < dimensiones.obtenerTilesHorizontalez(); x++) {
+        var r = new Rectangulo((x * dimensiones.ladoTiles) , (y * dimensiones.ladoTiles), dimensiones.ladoTiles, dimensiones.ladoTiles);
+        // var r = new Rectangulo(x * dimensiones.ladoTiles(), y * dimensiones.ladoTiles(), dimensiones.ladoTiles(), dimensiones.ladoTiles());
+      }
+    }
   }
-}
+};
