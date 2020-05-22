3d3c0931c24a4c7a331db443679f5b697f7dbd68
Gerardo González <wagegosa@gmail.com>
2020-05-22 15:25:50 -0500

    Se elimina un consoles.log ya que no lo necesitamos.

--- js/rectangulo.js
+++ js/rectangulo.js
@@ -12,7 +12,6 @@ Rectangulo.prototype.inssertarDOM =  function(){
   var html = document.getElementById("juego").innerHTML;
   var color = '#' + Math.floor(Math.random() * 16777215).toString(16);//toString convierte a un string
   document.getElementById("juego").innerHTML = html + div;
-  console.log(this.id);
   document.getElementById(this.id).style.position = "absolute";
   document.getElementById(this.id).style.left = this.x + "px";
   document.getElementById(this.id).style.top = this.y + "px";
