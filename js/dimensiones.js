ca7375535b75735b6ed2b8d6c9be9b41fd88b77f
Gerardo González <wagegosa@gmail.com>
2020-05-22 15:29:08 -0500

    Se crea llamdo de metodo inicio.recargarTiles y se creal 3 funciones con los que obtenemos los Tiles horizontales, verticales y el total de los 2 anteriores.

--- js/dimensiones.js
+++ js/dimensiones.js
@@ -1,12 +1,25 @@
 var dimensiones = {
-    ancho: window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth,
-    alto: window.innerHeight || document.documentElement.clientHeight ||document.body.clientHeight,
-    iniciar: function(){
-        window.addEventListener("resize", function
-        (e){
-            dimensiones.ancho = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-            dimensiones.alto = window.innerHeight || document.documentElement.clientHeight ||document.body.clientHeight;
-            console.log("Ancho: " + dimensiones.ancho + ", Alto: " + dimensiones.alto);
-        });
-    }
+  ancho: window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth,
+  alto: window.innerHeight || document.documentElement.clientHeight ||document.body.clientHeight,
+  ladoTiles: 100,
+  escala: 1,
+  iniciar: function(){
+    window.addEventListener("resize", function
+      (e){
+        dimensiones.ancho = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+        dimensiones.alto = window.innerHeight || document.documentElement.clientHeight ||document.body.clientHeight;
+        inicio.recargarTiles();
+      });
+  },
+  obtenerTilesHorizontalez: function(){
+    var ladoFinal = dimensiones.ladoTiles * dimensiones.escala;
+    return Math.ceil((dimensiones.ancho - ladoFinal ) / ladoFinal);
+  },
+  obtenerTilesVerticales: function(){
+    var ladoFinal = dimensiones.ladoTiles * dimensiones.escala;
+    return Math.ceil((dimensiones.alto - ladoFinal ) / ladoFinal);
+  },
+  obtenerTilesTotales: function(){
+    return dimensiones.obtenerTilesHorizontalez() * dimensiones.obtenerTilesVerticales();
+  }
 };
