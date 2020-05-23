69bf016067f7ecbe50097b4e0ac0233b165a9311
Gerardo González <wagegosa@gmail.com>
2020-05-22 19:52:41 -0500

    Se agrega llamado a evento llamado teclado.

--- js/inicio.js
+++ js/inicio.js
@@ -6,6 +6,7 @@ document.addEventListener('DOMContentLoaded', function () {
 var inicio = {
   iniciarJuego: function () {
     console.log("Juego iniciado");
+    teclado.go();
     dimensiones.iniciar()
     //var r = new Rectangulo(0, 0, 100, 100);
     // var r2 = new Rectangulo(100, 0, 100, 100);
